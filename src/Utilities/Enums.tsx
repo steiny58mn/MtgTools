028d5d0cb4337bca9e873a6be21cd0e19cd2856e
Andy Steinkopf <steiny58@gmail.com>
2026-09-18 12:21:39 -0500

    Update URL

--- src/Utilities/Enums.tsx
+++ src/Utilities/Enums.tsx
@@ -1,19 +1,22 @@
 import config from '../appsettings.json';
+
 export const ToolTypeCodes = {
     DeckUpdates: 1,
     SetReview: 2,
     GameSummary: 3
 } as const
 
+const baseUrl = import.meta.env.VITE_API_URL || (import.meta.env.PROD ? config.remoteurl : (config.localurl || config.url));
+
 export const apiPaths = {
-    Root: `${config.url}/${config.mtgtoolsendpoint}`,
-    GetBbCode: `${config.url}/${config.mtgtoolsendpoint}/getbbcode`,
-    FormatText: `${config.url}/${config.mtgtoolsendpoint}/formattext`,
-    DeckColors: `${config.url}/${config.mtgtoolsendpoint}/deckcolors`,
-    CreateDecklist: `${config.url}/${config.mtgtoolsendpoint}/createdecklist`,
-    UpdateDb: `${config.url}/${config.mtgtoolsendpoint}/updatedb`,
-    CompareFiles: `${config.url}/${config.mtgtoolsendpoint}/comparefiles`,
-    Test: `${config.url}/${config.mtgtoolsendpoint}/test`,
-    ParseMtgoLog: `${config.url}/${config.mtgtoolsendpoint}/parsemtgolog`,
-    CreateDeckPicklist: `${config.url}/${config.mtgtoolsendpoint}/createdeckpicklist`,
+    Root: `${baseUrl}/${config.mtgtoolsendpoint}`,
+    GetBbCode: `${baseUrl}/${config.mtgtoolsendpoint}/getbbcode`,
+    FormatText: `${baseUrl}/${config.mtgtoolsendpoint}/formattext`,
+    DeckColors: `${baseUrl}/${config.mtgtoolsendpoint}/deckcolors`,
+    CreateDecklist: `${baseUrl}/${config.mtgtoolsendpoint}/createdecklist`,
+    UpdateDb: `${baseUrl}/${config.mtgtoolsendpoint}/updatedb`,
+    CompareFiles: `${baseUrl}/${config.mtgtoolsendpoint}/comparefiles`,
+    Test: `${baseUrl}/${config.mtgtoolsendpoint}/test`,
+    ParseMtgoLog: `${baseUrl}/${config.mtgtoolsendpoint}/parsemtgolog`,
+    CreateDeckPicklist: `${baseUrl}/${config.mtgtoolsendpoint}/createdeckpicklist`,
 } as const
